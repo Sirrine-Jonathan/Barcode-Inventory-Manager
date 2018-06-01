d2a4964212405a19690543cad6b59feebbe6bcbf
brownm26 <empoleon207@gmail.com>
2018-06-01 15:59:58 -0600

    Added basic Room database.

--- app/src/main/java/byui_cs246/barcodeinventorymanager/ItemViewModel.java
+++ app/src/main/java/byui_cs246/barcodeinventorymanager/ItemViewModel.java
@@ -1,5 +1,24 @@
 package byui_cs246.barcodeinventorymanager;
 
-public class ItemViewModel
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ItemViewModel extends AndroidViewModel
 {
+    private ItemRepository mRepository;
+
+    private LiveData<List<Item>> mAllItems;
+
+    public ItemViewModel (Application application) {
+        super(application);
+        mRepository = new ItemRepository(application);
+        mAllItems = mRepository.getAllItems();
+    }
+
+    LiveData<List<Item>> getAllWords() { return mAllItems; }
+
+    public void insert(Item item) { mRepository.insert(item); }
 }
--- app/src/main/java/byui_cs246/barcodeinventorymanager/MainActivity.java
+++ app/src/main/java/byui_cs246/barcodeinventorymanager/MainActivity.java
@@ -1,49 +1,98 @@
 package byui_cs246.barcodeinventorymanager;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    public static final int NEW_ITEM_ACTIVITY_REQUEST_CODE = 1;
+
+    private ItemViewModel mItemViewModel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, NewItemActivity.class);
+                startActivityForResult(intent, NEW_ITEM_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.list);
+        final ItemListAdapter adapter = new ItemListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mItemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
+
+        mItemViewModel.getAllWords().observe(this, new Observer<List<Item>>()
+        {
+            @Override
+            public void onChanged(@Nullable final List<Item> items)
+            {
+                // Update the cached copy of the words in the adapter.
+                adapter.setItems(items);
             }
         });
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == NEW_ITEM_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            int id = data.getIntExtra(NewItemActivity.EXTRA_ID, 0);
+            String name = data.getStringExtra(NewItemActivity.EXTRA_NAME);
+            int quantity = data.getIntExtra(NewItemActivity.EXTRA_QUANTITY, 1);
+
+            Item item = new Item(id, name, quantity);
+            mItemViewModel.insert(item);
+        }
+    }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_settings)
+        {
             return true;
         }
 
